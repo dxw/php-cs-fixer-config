413ebc9f1c870e926680e7a0f7ad75a55f79b563
Rob Skilling <rob@dxw.com>
2022-09-23 17:23:16 +0100

    Apply the new PSR12/tab config to our own code

--- src/Config.php
+++ src/Config.php
@@ -4,13 +4,13 @@ namespace Dxw\PhpCsFixerConfig;
 
 class Config
 {
-    public static function create() : \PhpCsFixer\ConfigInterface
-    {
-        $config = new \PhpCsFixer\Config();
-        return $config->setRules([
-            '@PSR12' => true,
-            'array_syntax' => ['syntax' => 'short']
-        ])
-        ->setIndent("\t");
-    }
+	public static function create(): \PhpCsFixer\ConfigInterface
+	{
+		$config = new \PhpCsFixer\Config();
+		return $config->setRules([
+			'@PSR12' => true,
+			'array_syntax' => ['syntax' => 'short']
+		])
+		->setIndent("\t");
+	}
 }
